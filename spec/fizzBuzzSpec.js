0554f2f8e35576eb762521a19444c5622968a54b
Remy Manso <remzilla@Remys-MacBook-Pro.local>
2019-06-24 16:57:52 +0100

    We reversed TDD'd this solution. Perfect.

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -10,4 +10,22 @@ describe('FizzBuzz', function(){
       expect(fizzbuzz.game(3)).toEqual("Fizz");
     });
   });
+
+  describe('Multiples of 5', function(){
+    it('Buzz returned for multiples of 5', function(){
+      expect(fizzbuzz.game(5)).toEqual("Buzz");
+    });
+  });
+
+  describe('Multiples of 15', function(){
+    it('Buzz returned for multiples of 15', function(){
+      expect(fizzbuzz.game(15)).toEqual("FizzBuzz");
+    });
+  });
+
+  describe('If not divisible by any', function(){
+    it('returns input number', function(){
+      expect(fizzbuzz.game(2)).toEqual(2);
+    });
+  });
 });
